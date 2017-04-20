67c31410d3886060d7eefaacf1bf2a9991b34a75
a15kriel <a15kriel@hs.local>
2017-04-20 15:38:15 +0200

    Tända lampor

--- main.asm
+++ main.asm
@@ -1,13 +1,48 @@
 ;
 ; Snake.asm
 ;
-; Created: 2017-04-20 13:54:29
-; Author : DELL
+; Created: 2017-04-20 15:17:06
+; Author : a15kriel
 ;
 
 
 ; Replace with your application code
-start:
-    inc r16
-    rjmp start
-	Hejsan
+
+	.DEF rTemp				= r16
+	.DEF rNoll				= r17
+	.DEF rPORTB				= r20
+	.DEF rPORTC				= r21
+	.DEF rPORTD				= r22
+
+
+	ldi rTemp, 0b11111111
+	ldi rNoll, 0b00000000
+
+
+	out DDRB, rTemp
+	out DDRC, rTemp
+	out DDRD, rTemp
+
+
+	out PORTB, rNoll
+	out PORTC, rNoll
+	out PORTC, rNoll
+
+
+
+	
+main:
+
+	ldi rPORTB, 0b11111111
+	ldi rPORTC, 0b11111110
+	ldi rPORTD, 0b10111111
+	
+	out PORTB, rPORTB
+	out PORTC, rPORTC
+	out PORTD, rPORTD
+
+
+
+
+
+    rjmp main
